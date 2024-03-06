3cbad70b8a33c62314edfaf9dc676df35427f02d
HttpAnimation <shawndouglasyt@gmail.com>
2024-03-06 11:08:18 -0500

    removes old files

--- main.cpp
+++ main.cpp
@@ -1,11 +1,33 @@
-// main.cpp
+#include <TLTK.hpp>
 
-#include <QApplication>
-#include "mainwindow.h"
+int main() {
+    TLTK::Window window("Kilt IDE", TLTK::Vector2i(800, 600));
 
-int main(int argc, char *argv[]) {
-    QApplication app(argc, argv);
-    MainWindow mainWindow;
-    mainWindow.show();
-    return app.exec();
+    // Create a main page
+    TLTK::Page mainPage("Main Page");
+
+    // Add some widgets to the main page
+    mainPage.addWidget(new TLTK::Label("Welcome to Kilt IDE", TLTK::Vector2i(10, 10)));
+    mainPage.addWidget(new TLTK::Button("New File", TLTK::Vector2i(10, 50), [](){ 
+        // Code to handle creating a new file
+        std::cout << "Creating a new file...\n";
+    }));
+    mainPage.addWidget(new TLTK::Button("Open File", TLTK::Vector2i(10, 90), [](){ 
+        // Code to handle opening a file
+        std::cout << "Opening a file...\n";
+    }));
+    mainPage.addWidget(new TLTK::Button("Save File", TLTK::Vector2i(10, 130), [](){ 
+        // Code to handle saving a file
+        std::cout << "Saving a file...\n";
+    }));
+
+    // Add the main page to the window
+    window.addPage(mainPage);
+
+    // Main application loop
+    while (window.isOpen()) {
+        window.update();
+    }
+
+    return 0;
 }
