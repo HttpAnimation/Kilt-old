67b57cd852212092a4cb9c9d252d8d772b5cb825
HttpAnimation <shawndouglasyt@gmail.com>
2024-03-06 11:49:24 -0500

    compile

--- main.cpp
+++ main.cpp
@@ -51,7 +51,7 @@ void on_open_folder_clicked(GtkWidget *widget, gpointer data) {
 
         // Add files to the sidebar
         GtkTreeIter iter;
-        GtkTreeModel *model = gtk_tree_view_get_model(GTK_TREE_VIEW(data));
+        GtkTreeModel *model = GTK_TREE_MODEL(data);
         gtk_tree_store_clear(GTK_TREE_STORE(model));
         for (const auto &file : files) {
             gtk_tree_store_append(GTK_TREE_STORE(model), &iter, NULL);
@@ -85,8 +85,13 @@ int main(int argc, char *argv[]) {
     // Create buttons
     GtkWidget *button_open_folder = gtk_button_new_with_label("Open Folder");
 
+    // Create the tree view
+    GtkTreeStore *tree_store = gtk_tree_store_new(1, G_TYPE_STRING);
+    GtkWidget *tree_view = gtk_tree_view_new_with_model(GTK_TREE_MODEL(tree_store));
+    g_object_unref(tree_store); // Free tree store when the view is destroyed
+
     // Connect signals to button clicks
-    g_signal_connect(button_open_folder, "clicked", G_CALLBACK(on_open_folder_clicked), NULL);
+    g_signal_connect(button_open_folder, "clicked", G_CALLBACK(on_open_folder_clicked), tree_store);
 
     // Add buttons to the sidebar
     gtk_box_pack_start(GTK_BOX(sidebar_box), button_open_folder, FALSE, FALSE, 0);
@@ -95,11 +100,6 @@ int main(int argc, char *argv[]) {
     GtkWidget *scrolled_window = gtk_scrolled_window_new(NULL, NULL);
     gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scrolled_window), GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
     gtk_box_pack_start(GTK_BOX(box), scrolled_window, TRUE, TRUE, 0);
-
-    // Create the tree view
-    GtkTreeStore *tree_store = gtk_tree_store_new(1, G_TYPE_STRING);
-    GtkWidget *tree_view = gtk_tree_view_new_with_model(GTK_TREE_MODEL(tree_store));
-    g_object_unref(tree_store); // Free tree store when the view is destroyed
 
     GtkTreeViewColumn *column = gtk_tree_view_column_new_with_attributes("Files", gtk_cell_renderer_text_new(), "text", 0, NULL);
     gtk_tree_view_append_column(GTK_TREE_VIEW(tree_view), column);
